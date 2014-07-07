95797982d63da92c8ac7471ff0765a23e2f53ab1
Hakan Henriksson <haakan.henriksson@gmail.com>
2014-07-07 20:57:10 +0200

    Initial commit of tango_with_django_project

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Development/Desktop/Python/Projects/tango_with_django_project/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
